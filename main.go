3a7dea134c4fe404c271526988a13c0114b799bd
Hikaru Tomizawa <tomizawa.licht@gmail.com>
2025-02-04 23:47:10 +0900

    implement post method entrypoint

--- main.go
+++ main.go
@@ -13,6 +13,18 @@ type Todo struct {
 	Completed bool   `json:"completed"`
 }
 
+// 新しいTodoを作成するためのリクエスト構造体
+// IDは自動で付与
+type CreateTodoRequest struct {
+	Title     string `json: "title"`
+	Completed bool   `json: "completed"`
+}
+
+// エラーレスポンス用の構造体
+type ErrorResponse struct {
+	Message string `json: "message"`
+}
+
 // パッケージレベルで変数を定義（テストケース）
 var todos = []Todo{
 	{ID: 1, Title: "買い物に行く", Completed: false},
@@ -22,18 +34,36 @@ var todos = []Todo{
 const todoPath = "/api/todos"
 
 // パッケージレベルのハンドラ関数
-func getTodos(w http.ResponseWriter, r *http.Request) {
+func handleTodos(w http.ResponseWriter, r *http.Request) {
+
 	// レスポンスヘッダーの設定
 	w.Header().Set("Content-Type", "application/json")
 
-	// todosをjsonとしてエンコードしてレスポンスに書き込む
-	json.NewEncoder(w).Encode(todos)
+	switch r.Method {
+	case http.MethodGet:
+		// todosをjsonとしてエンコードしてレスポンスに書き込む
+		json.NewEncoder(w).Encode(todos)
+	case http.MethodPost:
+		var createRequest CreateTodoRequest
+		if err := json.NewDecoder(r.Body).Decode(&createRequest); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(ErrorResponse{
+				Message: "Invalid request body",
+			})
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			Message: "Method not allowed. User GET or POST instead",
+		})
+	}
 }
 
 func main() {
 	fmt.Printf("Registering handler for path: %s\n", todoPath)
 	// ルーティングの登録
-	http.HandleFunc(todoPath, getTodos)
+	http.HandleFunc(todoPath, handleTodos)
 
 	fmt.Println("Starting server on port 8080")
 
